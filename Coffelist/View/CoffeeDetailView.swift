ccc426758b830725d3ff7de5d0200bcd9c7a8242
Pratama Pangestu <p7atama@protonmail.ch>
2022-12-04 01:03:44 +0700

    adding package, favourite, share and tab

--- Coffelist/View/CoffeeDetailView.swift
+++ Coffelist/View/CoffeeDetailView.swift
@@ -7,95 +7,78 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct CoffeeDetailView: View {
-    let CoffeDetails: CoffeeModel
+    // MARK: - PROPERTIES
+    let coffeeDetails: CoffeeModel
     
+    // MARK: - BODY
     var body: some View {
-        ZStack{
-            LinearGradient(colors:[.white,.white,.blue],startPoint: .topLeading,endPoint: .bottomTrailing)
+        ZStack {
+            LinearGradient(colors: [.indigo, .white], startPoint: .bottom, endPoint: .top)
                 .ignoresSafeArea()
             
-            VStack(alignment: .center){
-                
-                Image(CoffeDetails.image)
+            VStack(alignment: .center, spacing: 0) {
+                Image(coffeeDetails.image)
                     .resizable()
                     .scaledToFill()
-                    .frame(height: 300)
+                    .frame(width: 250, height: 250)
                     .clipShape(Circle())
                     .overlay {
                         ZStack {
                             Circle()
-                                .opacity(0.6)
-
+                                .opacity(0.5)
+                            
                             VStack {
                                 Image(systemName: "person.crop.circle.fill.badge.plus")
-                                    .resizable()
                                     .renderingMode(.original)
-                                    .frame(height: 68)
-                                    .scaledToFit()
                                     .foregroundColor(.blue)
-                                    
-                                Text(CoffeDetails.name)
-                                    .font(.system(.title
-                                                 )
-                                        .bold())
+                                    .font(.system(.largeTitle, design: .rounded))
+                                    .fontWeight(.bold)
+                                Text(coffeeDetails.name)
+                                    .foregroundColor(.white)
+                                    .font(.system(.title, design: .rounded))
+                                    .fontWeight(.bold)
                             }
-                            .foregroundColor(.white)
-                            .font(.system(.title, design: .rounded))
-                            .fontWeight(.bold)
                         }
+                        
                     }
-                    
-                Text(CoffeDetails.review)
-                    .font(.system(size: 18, design: .rounded))
-                    .lineSpacing(6)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.blue)
                 
-                ZStack {
-                    HStack{
-                        Circle()
-                            .fill(.pink)
-                            .frame(width: 28, height: 30)
-                            .overlay {
-                                ZStack {
-                                    Circle()
-                                        .opacity(0)
-
-                                    VStack {
-                                        Image(systemName: "pin")
-                                            .resizable()
-                                            .renderingMode(.original)
-                                            .frame(width: 16, height: 20)
-                                            .scaledToFit()
-                                            .foregroundColor(.pink)
-                                            
-                                        
-                                    }
-                                    
-                                }
-                            }
+                VStack(alignment: .center, spacing: -20) {
+                    Text(coffeeDetails.review)
+                        .padding(30)
+                        .foregroundColor(.blue)
+                        .font(.system(.headline, design: .rounded))
+                        .fontWeight(.regular)
+                        .lineSpacing(4)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    HStack {
+                        Image(systemName: "pin.circle")
+                            .foregroundColor(.red)
+                            .font(.system(.title2))
                         
-                        Text(CoffeDetails.location)
-                            .font(.system(size: 18, design: .rounded))
-                            .lineSpacing(6)
+                        Text(coffeeDetails.location)
+                            .foregroundColor(.red)
+                            .font(.system(.callout, design: .rounded))
+                            .fontWeight(.light)
                             .multilineTextAlignment(.center)
-                            .foregroundColor(.pink)
-                        
                     }
                 }
-                    
                 Spacer()
-                
-               
             }
-            .padding()
+            
         }
+        
     }
 }
 
+// MARK: - PREVIEW
 struct CoffeeDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        CoffeeDetailView(CoffeDetails: CoffeeModel(image: "bojaeatery", name: "Boja Eatery", location: "Jl. Sumatera No. 21, Kota Bandung", review: "The food is delicious, cooked just right.  Friendly service.  Cozy atmosphere with floral decorations.  There is a ~robot~ that sends the dish.  Prices are reasonable for a restaurant of its class"))
+        CoffeeDetailView(coffeeDetails: CoffeeModel(
+            image: "bojaeatery", name: "Boja Eatery", location: "Jl. Sumatera No. 21, Kota Bandung", review: "The food is delicious, cooked just right.  Friendly service.  Cozy atmosphere with floral decorations.  There is a ~robot~ that sends the dish.  Prices are reasonable for a restaurant of its class", isFavorite: false))
     }
 }
--- Coffelist/View/CoffeeListView.swift
+++ Coffelist/View/CoffeeListView.swift
@@ -1,97 +1,180 @@
 //
-//  CoffeeListView.swift
+//  CoffeeDetailView.swift
 //  Coffelist
 //
-//  Created by Pratama Pangestu on 19/11/22.
+//  Created by Pratama Pangestu on 20/11/22.
 //
 
 import SwiftUI
+import CachedAsyncImage
+import ActivityView
 
 struct CoffeeListView: View {
     
-    @State private var searchText: String = ""
-    
-    private var coffeeSearchResults : [CoffeeModel] {
-        let results = CoffeeModel.CoffeeshopProvider.all()
-        if searchText.isEmpty {
-            return results
-        }
-        else{
-            return results.filter{
-                $0.name
-                    .lowercased()
-                    .contains(searchText.lowercased()) || $0.location.contains(searchText)
-            }
-        }
+    // MARK: - PROPERTIES
+    @State private var isLoading: Bool = true
+
+    private var coffeeResult: [CoffeeModel] {
+        let result = CoffeeModel.CoffeeProvider.all()
+        
+       
+        return result
     }
     
-    private var suggestedResult:[CoffeeModel]{
-        if searchText.isEmpty {
-            return []
-        }
-        else{
-            return coffeeSearchResults
-        }
-    }
-    
-    
+    // MARK: - BODY
     var body: some View {
-        NavigationStack{
-            List(coffeeSearchResults) { Result in
-                NavigationLink(destination: {
-                    CoffeeDetailView(CoffeDetails: Result)
-                }){
-                    HStack{
-                        Image(Result.image)
-                            .resizable()
-                            .scaledToFit()
-                            .cornerRadius(15)
-                            .frame(width: 100, height: 100)
-                        VStack{
-                            HStack{
-                                Text(Result.name)
-                                    .font(.system(.title2
-                                                 )
-                                        .bold())
-                                Spacer()
-                            }
-                            .padding(1)
+        NavigationStack {
+            
+            List {
+                ForEach(coffeeResult) { result in
+                    ZStack(alignment: .leading) {
+                        NavigationLink(destination: {
+                            CoffeeDetailView(coffeeDetails: result)
+                        }) {
+                            EmptyView()
                             
-                            HStack{
-                                Text(Result.location)
-                                    .font(.system(size: 16)
-                                        .bold())
-                                    .foregroundColor(.gray)
-                                Spacer()
-                            }
-                            .padding(1)
                             
-                            HStack{
-                                Text("Rating: 4/5")
-                                    .font(.system(size: 14))
-                                    .foregroundColor(.gray)
-                                Spacer()
-                            }
                         }
-                    }
+                        .opacity(0)
+                        
+                        CoffeeRow(viewModel: result)
+        
+                    } //: ZSTACK
+                    
                 }
-            }
-            .navigationTitle("Coffeeshop")
-            .searchable(text: $searchText,
-                        placement: .navigationBarDrawer(displayMode: .always),
-            prompt: "What Coffee that you're looking for?")
-            {
-                ForEach(suggestedResult) { result in
-                    Text("Looking for \(result.name)?")
-                        .searchCompletion(result.name)
+                .onDelete { indexSet in
+                
                 }
+                
+                .redacted(reason: isLoading ? .placeholder : [])
+                
+                .onAppear {
+                    fetchData()
+                }
+                .listRowSeparator(.hidden)
+                
             }
-        }//NavStack
+            .refreshable {
+                print("Content Refresh")
+            }
+            
+            .listStyle(.plain)
+            
+        } //: NavigationStack
+        .navigationTitle("Coffee Shop")
+        
+    } // MARK : NAVIGATION
+    
+    // MARK: - Function
+    func fetchData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            isLoading = false
+        }
     }
 }
 
-struct CoffeeListView_Previews: PreviewProvider {
+// MARK: - PREVIEW
+struct CoffeeshopListView_Previews: PreviewProvider {
     static var previews: some View {
         CoffeeListView()
     }
+}
+
+// MARK: - SUBVIEW
+struct CoffeeRow: View {
+    @State var viewModel: CoffeeModel
+    @State private var item: ActivityItem?
+    @State private var isShowingAlert = false
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 15) {
+            Image("\(viewModel.image)")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+                .frame(width: 120, height: 120)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text("\(viewModel.name)")
+                    .font(.callout)
+                    .fontWeight(.bold)
+                    .padding(2)
+                
+                Text("\(viewModel.location)")
+                    .font(.callout)
+                    .foregroundColor(.gray)
+                    .fontWeight(.bold)
+                    .padding(2)
+                
+                Text("Rating: 4/5")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .fontWeight(.light)
+                    .padding(2)
+            }
+            
+            VStack(alignment: .leading) {
+                if viewModel.isFavorite {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.yellow)
+                        .padding(.leading)
+                }
+                
+            } //: VStack
+            
+        } //: HSTACK
+        
+        .swipeActions(edge: .leading, allowsFullSwipe: false) {
+            Button {
+                viewModel.isFavorite.toggle()
+            } label: {
+                viewModel.isFavorite ? Image(systemName: "heart.slash") : Image(systemName: "heart")
+            }
+            .tint(.green)
+            
+            Button {
+                item = ActivityItem(items: "Coffee Shop with name: \(viewModel.name) will be shared!")
+            } label: {
+                Image(systemName: "square.and.arrow.up")
+            }
+            .tint(.indigo)
+        }
+        
+        .alert(isPresented: $isShowingAlert) {
+            Alert(title: Text("Not yet Available"), message: Text("This feature is not available yet"), dismissButton: .default(Text("OK")))
+        }
+        
+        .contextMenu {
+            Button {
+                isShowingAlert.toggle()
+            } label: {
+                HStack {
+                    Text("Pin this location")
+                    Image(systemName: "pin")
+                }
+            }
+           
+
+            Button {
+                viewModel.isFavorite.toggle()
+            } label: {
+                Text(viewModel.isFavorite ? "Remove from favorites" : "Mark as favorites")
+                viewModel.isFavorite ? Image(systemName: "heart.slash") : Image(systemName: "heart")
+            }
+            
+            Button {
+                item = ActivityItem(items: "Coffee Shop with name: \(viewModel.name) will be shared!")
+            } label: {
+                HStack {
+                    Text("Share")
+                    Image(systemName: "square.and.arrow.up")
+                }
+            }
+        }
+        .activitySheet($item)
+    }
+    
 }
